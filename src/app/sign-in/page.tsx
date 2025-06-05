55d3fb0264ce634fc03ea1f82be9bd66c1bed358
VedantRGosavi <vedantrgosavi@gmail.com>
2025-06-05 16:31:00 -0400

    fixed auth.

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -4,8 +4,14 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function SignInPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-phalo-green">
-      <SignIn />
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <SignIn
+        routing="path"
+        path="/sign-in"
+        signUpUrl="/sign-up"
+        afterSignInUrl="/dashboard"
+        redirectUrl="/dashboard"
+      />
     </div>
   );
 } 
